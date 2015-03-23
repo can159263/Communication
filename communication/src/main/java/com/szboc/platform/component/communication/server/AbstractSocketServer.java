a32dc5fa8ca256aeef9eeaf4d72e684e70b8e291
tomcan <tomcan@yeah.net>
2015-03-23 11:21:53 +0800

    初始版本导入

---
+++ communication/src/main/java/com/szboc/platform/component/communication/server/AbstractSocketServer.java
@@ -0,0 +1,17 @@
+package com.szboc.platform.component.communication.server;
+
+public abstract class AbstractSocketServer implements IServer {
+
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void shutdown() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public abstract IHandler getHandler();
+
+}
